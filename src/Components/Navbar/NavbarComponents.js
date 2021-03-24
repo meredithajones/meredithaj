a361708ac06488e45a5a9e811c349c4f40d5de3e
Meredith Jones <mj19003@gmail.com>
2021-03-24 15:49:54 -0400

    Added navbar style components

---
+++ src/Components/Navbar/NavbarComponents.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+    background: #000;
+    height: 80px;
+    margin-top: -80px;
+    display: flex;
+    justify-content: center;
+    alighn-items: center;
+    font-size: 1rem;
+    positions: sticky;
+    top: 0;
+    z-index: 10;
+    `
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+          <Nav>
+              <NavbarContainer>
+                  <NavLogo>
+                      
+                  </NavLogo>
+              </NavbarContainer>
+
+          </Nav>
+            
+        </>
+    );
+};
+
+export default Navbar
