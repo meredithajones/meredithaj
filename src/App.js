24e4e1b7c677511f1e654edb8db9822907228145
Meredith Jones <mj19003@gmail.com>
2021-03-25 10:54:19 -0400

    Added mobile responsiveness & styling to navbar:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
     <Navbar /> 
-    </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements'
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem,NavLinks } from './NavbarElements'
 
 const Navbar = () => {
     return (
         <>
           <Nav>
               <NavbarContainer>
-                  <NavLogo>
+                  <NavLogo to='/'>
                       That Link
                   </NavLogo>
+                  <h1> Hellloo </h1>
+                  <MobileIcon>
+                      <FaBars />
+                  </MobileIcon>
+                  <NavMenu>
+                      <NavItem>
+                          <NavLinks to="about">About</NavLinks>
+                      </NavItem>
+                  </NavMenu>
               </NavbarContainer>
 
           </Nav>
