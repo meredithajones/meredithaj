69eccacd4c79d8f640987cba33083752addc6feb
Meredith Jones <mj19003@gmail.com>
2021-03-29 12:53:57 -0400

    Added links and routes to sidenav

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import {SidebarContainer, Icon, CloseIcon} from './SidebarElements';
+import React from "react";
+import { SidebarContainer, Icon, CloseIcon } from "./SidebarElements";
 
 const Sidebar = () => {
-    return (
-        <SidebarContainer>
-            <Icon>
-                <CloseIcon />
-            </Icon>
-            
-        </SidebarContainer>
-    )
-}
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">
+              About
+          </SidebarLink>
+            <SidebarLink to="discover">
+                Discover</SidebarLink>
+                <SidebarLink to="services">
+                    Services</SidebarLink>
+                <SidebarLink to="contact">
+                    Contact</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+            <SidebarRoute to='/signin'>
+                Sign In
+            </SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
