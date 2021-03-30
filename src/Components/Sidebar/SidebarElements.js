95cc225c6ced71ae7eba6383ea2b61fdd479a454
Meredith Jones <mj19003@gmail.com>
2021-03-30 18:03:20 -0400

    Set up imports for sidebarElements

--- src/Components/Sidebar/SidebarElements.js
+++ src/Components/Sidebar/SidebarElements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-
+import {Link as LinkS} from 'react-scroll';
+import {Link as LinkR} from 'react-router-dom'
 import {FaTimes} from 'react-icons/fa'
 
 export const SidebarContainer = styled.aside`
@@ -44,6 +45,35 @@ export const SidebarLink = styled(LinkS)`
     list-style: none;
     transition: 0.2s ease-in-out;
     text-decoration: none;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover{
+        color: #01bf71;
+        transition: 02s ease-in-out 
+    }
+    `
+
+    export const SideBtnWrap = styled.div`
+    display: flex;
+    justify-content: center;
+    `
+    export const SideBarRoute = styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 16px 64px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover{
+        transition: all 0.2 
+    }
     `
 
 
