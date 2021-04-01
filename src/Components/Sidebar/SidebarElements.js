e2883b83d95d1d8f888fabf3cb59693c2774be43
Meredith Jones <mj19003@gmail.com>
2021-04-01 15:09:37 -0400

    fixing typos in sidebarElements

--- src/Components/Sidebar/SidebarElements.js
+++ src/Components/Sidebar/SidebarElements.js
@@ -10,7 +10,7 @@ export const SidebarContainer = styled.aside`
     height: 100%;
     background: 0d0d0d;
     display: grid;
-    alighn-items: center;
+    align-items: center;
     top: 0;
     left: 0;
     transition: 0.3s ease-in-out;
@@ -24,8 +24,8 @@ export const CloseIcon = styled(FaTimes)`
 export const Icon = styled.div`
     position: absolute;
     top: absolute;
-    right: 1.5rem;
-    background: transparent;
+    right: 1.1rem;
+    background: 5F9EA0;
     font-size: 2rem;
     cursor: pointer;
     outline: none;
@@ -43,25 +43,24 @@ export const SidebarMenu = styled.ul`
     text-align: center;
 
     @media screen and (max-width: 480px) {
-        grid-templare-rows: repeat(6, 60px);
+        grid-template-rows: repeat(6, 60px);
     }
     `;
 
 export const SidebarLink = styled(LinkS)`
     display: flex;
     align-items: center;
-    justify-content: cener;
+    justify-content: center;
     font-size: 1.5rem;
     text-decoration: none;
     list-style: none;
     transition: 0.2s ease-in-out;
-    text-decoration: none;
     color: #fff;
     cursor: pointer;
 
     &:hover{
         color: #01bf71;
-        transition: 02s ease-in-out 
+        transition: 0.2s ease-in-out 
     }
     `
 
@@ -69,7 +68,7 @@ export const SidebarLink = styled(LinkS)`
     display: flex;
     justify-content: center;
     `
-    //
+
     export const SidebarRoute = styled(LinkR)`
     border-radius: 50px;
     background: #01bf71;
