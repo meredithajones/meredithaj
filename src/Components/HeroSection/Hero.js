086074226c1f3df5a160d5adbbb374615049bf62
Meredith Jones <mj19003@gmail.com>
2021-04-02 10:04:18 -0400

    Added styling & responsive elements to HeroContent and HeroH1

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { HeroContainer, HeroBg} from './HeroElements'
 
 const HeroSection = () => {
     return (
         <HeroContainer id ="home">
             <HeroBg>
-                
+                <HeroContent>
+                    <HeroH1>Hi! I'm Meredith, Developer & UX Designer</HeroH1>
+                        <HeroP>
+                          I create online spaces to help solve problems!  
+                        </HeroP>
+                        <HeroBtnWrapper></HeroBtnWrapper>
+                        <Button to="signup"> 
+                            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                        </Button>
+                    
+
+                </HeroContent>
             </HeroBg>
             
         </HeroContainer>
--- src/Components/HeroSection/HeroElements.js
+++ src/Components/HeroSection/HeroElements.js
@@ -11,4 +11,38 @@ export const HeroContainer =styled.div`
     z-index: 1;
 `
 
+export const HeroBg = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+`;
+
+export const HeroContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+export const HeroH1 = styled.h1`
+    color: #fff;
+    font-size: 48px;
+    text-align: center;
+
+    @media screen and(max-width: 768px){
+        font-size: 32px;
+    } 
+`
+export const HeroP = styled.p`
+`
+
 
