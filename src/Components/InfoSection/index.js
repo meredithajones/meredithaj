13c773d658bb60e887f449527bd9c9488c0680d2
Meredith Jones <mj19003@gmail.com>
2021-04-03 16:49:42 -0400

    Added & outlined InfoElements component

---
+++ src/Components/InfoSection/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const InfoSection = () => {
+    return (
+        <>
+        <InfoContainer id={id}>
+            <InfoWrapper>
+                <InfoRow>
+                    <Column1>
+                    <TextWrapper>
+                        <TopLine>
+                            TopLine
+                        </TopLine>
+                        <Heading>Heading</Heading>
+                        <SubTitle>SubTitle</SubTitle>
+                        <BtnWrap>
+                            <Button to="home">Button</Button>
+                        </BtnWrap>
+                    </TextWrapper>
+                    </Column1>
+                </InfoRow>
+            </InfoWrapper>
+            </InfoContainer>
+            
+        </>
+    )
+}
+
+export default InfoSection
