69ea20a811214604272fd6498b316173985329d8
Meredith Jones <mj19003@gmail.com>
2021-04-06 11:33:03 -0400

    Fixed background olor

--- src/Components/InfoSection/index.js
+++ src/Components/InfoSection/index.js
@@ -27,6 +27,9 @@ const InfoSection = ({
   buttonLabel,
   img,
   alt,
+  primary, 
+  dark, 
+  dark2
 }) => {
   return (
     <>
@@ -45,7 +48,11 @@ const InfoSection = ({
                     spy={true}
                     exact="true"
                     offset={-80}
-                    
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+
+
                   >{buttonLabel}</Button>
                 </BtnWrap>
               </TextWrapper>
