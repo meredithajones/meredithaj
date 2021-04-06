556c3273dbe7a203bff9fd88aedb5f72f204296e
Meredith Jones <mj19003@gmail.com>
2021-04-06 11:22:51 -0400

    Added react-scroll settings to button

--- src/Components/InfoSection/index.js
+++ src/Components/InfoSection/index.js
@@ -1,38 +1,65 @@
-import React from 'react'
-import { Button } from 'react-scroll'
-import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, 
-    TopLine, Heading, SubTitle, BtnWrap, Img, ImgWrap } from './InfoElements'
+import React from "react";
+import { Button } from "../ButtonElements";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  SubTitle,
+  BtnWrap,
+  Img,
+  ImgWrap,
+} from "./InfoElements";
 
-const InfoSection = () => {
-    return (
-        <>
-        <InfoContainer lightBg={lightBg} id={id} >
-            <InfoWrapper>
-                <InfoRow imgStart={imgStart}>
-                    <Column1>
-                    <TextWrapper>
-                        <TopLine>
-                            {topLine}
-                        </TopLine>
-                        <Heading lightText={lightText}>{headline}</Heading>
-                        <SubTitle darkText={darkText}>{description}</SubTitle>
-                        <BtnWrap>
-                            <Button to="home" >{buttonLabel}</Button>
-                        </BtnWrap>
-                    </TextWrapper>
-                    </Column1>
-                    <Column2>
-                        <ImgWrap>
-                        <Img src={img} alt={alt}/>
-                        </ImgWrap>
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  buttonLabel,
+  img,
+  alt,
+}) => {
+  return (
+    <>
+      <InfoContainer lightBg={lightBg} id={id}>
+        <InfoWrapper>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <SubTitle darkText={darkText}>{description}</SubTitle>
+                <BtnWrap>
+                  <Button to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
                     
-                    </Column2>
-                </InfoRow>
-            </InfoWrapper>
-            </InfoContainer>
-            
-        </>
-    )
-}
+                  >{buttonLabel}</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={Img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
 
-export default InfoSection
+export default InfoSection;
