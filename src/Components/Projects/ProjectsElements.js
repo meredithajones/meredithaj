9932e1e8f0f195ce61f7e22c7c573d2f62bfd5fb
Meredith Jones <mj19003@gmail.com>
2021-04-06 18:53:59 -0400

    Added styling and media queries to ProjectsWrapper

--- src/Components/Projects/ProjectsElements.js
+++ src/Components/Projects/ProjectsElements.js
@@ -11,5 +11,22 @@ export const ProjectsContainer = styled.div`
     @media screen and (max-width: 480px) {
         height: 1300px;
     }
+`
+export const ProjectsWrapper = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    grid-gap: 16px;
+    padding: 0 50px;
 
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        padding: 0 20px;
+    }
 `
