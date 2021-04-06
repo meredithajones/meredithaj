b4a3f918a2c1c0b7d0f509593746d02b0d9a4ac4
Meredith Jones <mj19003@gmail.com>
2021-04-06 10:00:18 -0400

    Added styling to TopLine and Heading

--- src/Components/InfoSection/InfoElements.js
+++ src/Components/InfoSection/InfoElements.js
@@ -50,3 +50,27 @@ export const InfoRow = styled.div `
         padding-top: 0;
         padding-bottom: 60px;
     `
+
+    export const TopLine = styled.p`
+        color: #01bf71;
+        font-size: 16px;
+        line-height: 16px;
+        font-weight: 700;
+        letter-spacing: 1.4pz;
+        text-transform: uppercase;
+        margin-bottom: 16px;
+    `
+    export const Heading = styled.h1`
+        margin-bottom: 24px;
+        font-size: 48px;
+        line-height: 1.1;
+        font-weight: 600;
+        color: ${({ lightText}) => (lightText ? '#f7f8fa' : '010606')
+    };
+
+    @media screen and (max-width: 480px) {
+        font-size: 32px;
+    }
+    `
+    export const Subtitle = styled.p`
+    `
