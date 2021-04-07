1fab95cc11efaccda87f975db89c3734dc537d04
Meredith Jones <mj19003@gmail.com>
2021-04-07 09:04:43 -0400

    Added styling to project cards and project icon

--- src/Components/Projects/ProjectsElements.js
+++ src/Components/Projects/ProjectsElements.js
@@ -30,3 +30,26 @@ export const ProjectsWrapper = styled.div`
         padding: 0 20px;
     }
 `
+
+export const ProjectsCard = styled.div`
+    background: #fff;
+    display: flex;
+    flex-direction: flex-start;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 340px;
+    padding: 30px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+    transition: all 0.2s ease-in-out;
+
+    &:hover{
+        transform: scale(1.02);
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+    }
+`
+export const ProjectsIcon = styled.img`
+    height: 160px;
+    width: 160px;
+    margin-bottom: 10px;
+`
