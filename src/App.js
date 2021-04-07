2d3b9f83228da39f775787d1ec0f5065c806baef
Meredith Jones <mj19003@gmail.com>
2021-04-07 13:27:22 -0400

    Implemented React router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Home from './Pages/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+          <Route path= "/" component ={Home} exact />
+      </Switch>
     </Router>
   );
 }
