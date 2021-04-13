ef4fa1b65d1b5529bcc7b74e876525d222b6a75a
Meredith Jones <mj19003@gmail.com>
2021-04-13 14:48:01 -0400

    Setting up email contact page, installed emailjs and react-toastify packages

---
+++ src/Components/SideButton/ContactForm/ContactForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ToastContainer, toast , Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../Pages/ContactPage.css';
+import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
+init("user_rMuPcHScdQ7joaXqMmqZr");
