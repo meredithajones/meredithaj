0c4f37dec23d6dbfe453fc91482b38be638135c5
Meredith Jones <mj19003@gmail.com>
2021-04-13 14:52:25 -0400

    Setting up Toastify success message on email form

--- src/Components/SideButton/ContactForm/ContactForm.js
+++ src/Components/SideButton/ContactForm/ContactForm.js
@@ -4,4 +4,22 @@ import 'react-toastify/dist/ReactToastify.css';
 import '../Pages/ContactPage.css';
 import emailjs from 'emailjs-com';
 import{ init } from 'emailjs-com';
+import { MdRotateLeft, MdRssFeed } from 'react-icons/md';
 init("user_rMuPcHScdQ7joaXqMmqZr");
+
+function ContactMe() {
+
+    const toastifySuccess = () => {
+        toast('Thanks for your email! I will get right back to you', {
+          position: 'bottom-right',
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: false,
+          className: 'submit-feedback success',
+          toastId: 'notifyToast'
+        });
+      };
+
+      export default ContactMe;
