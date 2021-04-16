d481b16d27776849a0548d53ce3df66c5de34a21
Meredith Jones <mj19003@gmail.com>
2021-04-16 15:04:38 -0400

    Outlined Expanded Projects File

--- src/Components/ExpandedProjects/ExpandedProject.js
+++ src/Components/ExpandedProjects/ExpandedProject.js
@@ -2,26 +2,16 @@ import React from "react";
 
 const ExpandedProject = ({isOpen, toggle}) => {
   return (
-    <SidebarContainer isOpen={isOpen} onClick={toggle}>
-      <Icon onClick={toggle}>
+    <ProjectsContainer isOpen={isOpen} onClick={toggle}>
+      <ExpandIcon onClick={toggle}>
         <CloseIcon />
-      </Icon>
-      <SidebarWrapper>
-        <SidebarMenu>
-        <SidebarLink to="home" onClick={toggle}>
-                    Home</SidebarLink>
-          <SidebarLink to="about" onClick={toggle}>
-              About
-          </SidebarLink>
-            <SidebarLink to="projects" onClick={toggle}>
-                Projects</SidebarLink>
-                <SidebarLink to="writing" onClick={toggle}>
-                Published Articles</SidebarLink>
-                <SidebarLink to="contact" onClick={toggle}>
-                    Contact</SidebarLink>
-        </SidebarMenu>
-      </SidebarWrapper>
-    </SidebarContainer>
+      </ExpandIcon>
+      <ProjectsWrapper>
+        <ProjectsMenu>
+        <ProjectsLink> </ProjectsLink>
+        </ProjectsMenu>
+      </ProjectsWrapper>
+    </ProjectsContainer>
   );
 };
 
